38d92990db43a9e349dc53a8c00153294e4aa474
Kshipra <kshiprakode@gmail.com>
2016-05-29 23:38:58 -0400

    Adding ex1.c

---
+++ ex1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(int argc, char *argv[])
+{
+	puts("Hello World.");
+	puts("Hi there!");
+	puts("I am trying to programm in C");
+	puts("Its been great till now");
+	puts("I like C");
+	return 0;
+}
